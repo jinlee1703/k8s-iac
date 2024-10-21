b86654c1a1fdca9322dd582acc5b3461197141cc
Jinwoo Lee <binaryWooo@gmail.com>
2024-10-22 00:11:24 +0900

    feat: iam 역할 정의 #8

--- modules/api/main.tf
+++ modules/api/main.tf
@@ -12,7 +12,7 @@ resource "aws_eks_cluster" "api" {
 resource "aws_eks_node_group" "api" {
   cluster_name    = aws_eks_cluster.api.name
   node_group_name = "${var.prefix}-api-node-group"
-  node_role_arn   = aws_iam_role.node.arn
+  node_role_arn   = aws_iam_role.node_group.arn
   subnet_ids      = var.subnet_ids
 
   scaling_config {
@@ -28,4 +28,59 @@ resource "aws_eks_node_group" "api" {
     aws_iam_role_policy_attachment.eks_node_group_AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.eks_node_group_AmazonEC2ContainerRegistryReadOnly,
   ]
+}
+
+resource "aws_iam_role" "api" {
+  name = "${var.prefix}-api-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.name
+}
+
+resource "aws_iam_role" "eks_node_group" {
+  name = "${var.prefix}-eks-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node_group.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node_group.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_group_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node_group.name
 }
