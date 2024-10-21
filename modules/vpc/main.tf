09937fbe8e81c854e0d48aac7ca30095e55078de
Jinwoo Lee <binaryWooo@gmail.com>
2024-10-21 21:53:06 +0900

    feat: subnet별 route table 리소스 정의 및 연결 설정 #6

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -88,3 +88,72 @@ resource "aws_nat_gateway" "main" {
     }
   )
 }
+
+resource "aws_route_table" "net" {
+  vpc_id = aws_vpc.main.id
+  count  = length(var.availability_zones)
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.prefix}-rtb-net"
+    }
+  )
+}
+
+resource "aws_route_table_association" "net" {
+  count          = length(var.availability_zones)
+  subnet_id      = aws_subnet.net[count.index].id
+  route_table_id = aws_route_table.net[count.index].id
+}
+
+resource "aws_route_table" "app" {
+  vpc_id = aws_vpc.main.id
+  count  = length(var.availability_zones)
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main[count.index].id
+  }
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.prefix}-rtb-app"
+    }
+  )
+}
+
+resource "aws_route_table_association" "app" {
+  count          = length(var.availability_zones)
+  subnet_id      = aws_subnet.app[count.index].id
+  route_table_id = aws_route_table.app[count.index].id
+}
+
+resource "aws_route_table" "data" {
+  vpc_id = aws_vpc.main.id
+  count  = length(var.availability_zones)
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main[count.index].id
+  }
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.prefix}-rtb-data"
+    }
+  )
+}
+
+resource "aws_route_table_association" "data" {
+  count          = length(var.availability_zones)
+  subnet_id      = aws_subnet.data[count.index].id
+  route_table_id = aws_route_table.data[count.index].id
+}
