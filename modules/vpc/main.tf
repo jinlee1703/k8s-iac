d5d6e6d03740c81e4df6f03a8ca15e79b056fd5e
Jinwoo Lee <binaryWooo@gmail.com>
2024-10-21 21:46:21 +0900

    feat: nat-gateway 및 eip 리소스 정의 #6

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -63,3 +63,28 @@ resource "aws_internet_gateway" "main" {
     }
   )
 }
+
+resource "aws_eip" "nat" {
+  count = length(var.availability_zones)
+  vpc   = true
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.prefix}-nat-eip/az${count.index + 1}"
+    }
+  )
+}
+
+resource "aws_nat_gateway" "main" {
+  count         = length(var.availability_zones)
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.net[count.index].id
+
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.prefix}-nat-gw/az${count.index + 1}"
+    }
+  )
+}
